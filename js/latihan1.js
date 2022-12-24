17b1aaa4aec549750c2ea8239e92e5298eb0b00f
Nissaaprilia <nissa.2904az@gmail.com>
2022-12-25 00:43:47 +0800

    kuis selesai

--- js/latihan1.js
+++ js/latihan1.js
@@ -1,158 +1,7 @@
-function ftampil(){
-	/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-	var dropdown = document.getElementsByClassName("dropdown-btn");
-	var i;
-
-	for (i = 0; i < dropdown.length; i++) {
-		console.log(dropdown.length);
-  		dropdown[i].addEventListener("click", function() {
-  			this.classList.toggle("active");
-  			var dropdownContent = this.nextElementSibling;
-  			if (dropdownContent.style.display === "block") {
-  				dropdownContent.style.display = "none";
-  			} else {
-  				dropdownContent.style.display = "block";
-  			}
-  		});
-	}
-}
-function dafis(){
-  let dafis = document.getElementById("dafis");
-  let main = document.getElementById("main");
-
-  if (dafis.className.indexOf("dafis-hilang") == -1){
-    dafis.className += "dafis-hilang";
-    main.className = main.className.replace("main","main-penuh");
-  } else{
-    dafis.className = dafis.className.replace("dafis-hilang","");
-    main.className = main.className.replace("main-penuh","main");
-  }
-}
-
-//top button
-// When the user scrolls down 20px from the top of the document,we will show the button
-window.onscroll = function(){
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-    document.getElementById("js-top").style.display = "block";
-  } else{
-    document.getElementById("js-top").style.display = "none";
-  }
-};
-
-function scrollToTop() {
-    var position =
-        document.body.scrollTop || document.documentElement.scrollTop;
-    if (position) {
-        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-        scrollAnimation = setTimeout("scrollToTop()", 30);
-    } else clearTimeout(scrollAnimation);
-}
-
-
-function tujuan(tujuan) {
-    var x = document.getElementById(tujuan);
-  
-    if (x.className.indexOf("hilang") == -1) {
-      x.className += " hilang";
-    } else {
-      x.className = x.className.replace("hilang", "");
-    }
-  }
-  
-  function apersepsi(apersepsi) {
-    var x = document.getElementById(apersepsi);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-  
-  function chlorella(chlorella) {
-    var x = document.getElementById(chlorella);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-  
-  function chlamydomonas(chlamydomonas) {
-    var x = document.getElementById(chlamydomonas);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  function hydrodictyon(hydrodictyon) {
-    var x = document.getElementById(hydrodictyon);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  function volvox(volvox) {
-    var x = document.getElementById(volvox);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  function spirogyra(spirogyra) {
-    var x = document.getElementById(spirogyra);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  function chlorococcum(chlorococcum) {
-    var x = document.getElementById(chlorococcum);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  function ulva(ulva) {
-    var x = document.getElementById(ulva);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  function chara(chara) {
-    var x = document.getElementById(chara);
-  
-    if (x.className.indexOf("lost") == -1) {
-      x.className += " lost";
-    } else {
-      x.className = x.className.replace("lost", "");
-    }
-  }
-
-  
-	
-	//cek jawaban isian
+//cek jawaban isian
 	function cekJawaban(){
+    //isian 1 
+    // let tidak dapat di deklarasikan ulang
     let isi1 = document.getElementById("isian1").value.toLowerCase();
     let jawaban1 = new RegExp('aksial');
     let result1 = jawaban1.test(isi1);
@@ -167,6 +16,7 @@ function tujuan(tujuan) {
         document.getElementById("isian1").style.border = "2px solid red";
     }
 
+    //isian 2
     let isi2 = document.getElementById("isian2").value.toLowerCase();
     let jawaban2 = new RegExp('apendikular');
     let result2 = jawaban2.test(isi2);
@@ -181,6 +31,7 @@ function tujuan(tujuan) {
         document.getElementById("isian2").style.border = "2px solid red";
     }
 
+    //isian 3
     let isi3 = document.getElementById("isian3").value.toLowerCase();
     let jawaban3 = new RegExp('aktif');
     let result3 = jawaban3.test(isi3);
@@ -195,6 +46,7 @@ function tujuan(tujuan) {
         document.getElementById("isian3").style.border = "2px solid red";
     }
 
+    //isian 4
     let isi4 = document.getElementById("isian4").value.toLowerCase();
     let jawaban4 = new RegExp('gelang panggul');
     let result4 = jawaban4.test(isi4);
@@ -209,6 +61,7 @@ function tujuan(tujuan) {
         document.getElementById("isian4").style.border = "2px solid red";
     }
 
+    //isian 5
     let isi5 = document.getElementById("isian5").value.toLowerCase();
     let jawaban5 = new RegExp('tubuh');
     let result5 = jawaban5.test(isi5);
@@ -223,6 +76,8 @@ function tujuan(tujuan) {
         document.getElementById("isian5").style.border = "2px solid red";
     }
   }
+
+  //reset isian
   function reset(){
     // document.getElementsByClassName('ini')[0].value = "";
     document.getElementById("isian1").value = "";
